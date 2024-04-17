dad6f41386233f01f67c82138654ad9388a3980c
alexDanilMain <alexdanilwork@gmail.com>
2024-04-17 11:50:43 +0200

    migrates old controllers and models

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,11 +1,23 @@
+using System.Reflection;
+using Backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+var config = new ConfigurationBuilder()
+.AddJsonFile("appsettings.json", false, true)
+.AddUserSecrets(Assembly.GetExecutingAssembly(), true)
+.Build();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<CanineContext>(options =>
+    options.UseSqlServer(config.GetConnectionString("CanineContext") ?? throw new InvalidOperationException("Connection string 'CanineContext' not found.")));
+
 
 var app = builder.Build();
 
