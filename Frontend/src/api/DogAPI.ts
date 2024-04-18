c4c62eb4ecc6583b730b0b1d2a26be84c3cec3f0
Ewy Rostam <ewy.rost@gmail.com>
2024-04-18 21:51:27 +0200

    Add dog api in frontend

---
+++ Frontend/src/api/DogAPI.ts
@@ -0,0 +1,14 @@
+type Dog = {
+    name: string;
+    birthYear: number;
+    imageUrl: string;
+}
+
+
+const URL_BASE = "http://http://localhost:5092/Dogs";
+const headers = {'Content-type': "application/json; charset=UTF-8"}
+
+export const getAllDogs = async (): Promise<Dog[]> => {
+    const result = await fetch(URL_BASE).then(result => result.json())
+    return result as Dog[];
+}
