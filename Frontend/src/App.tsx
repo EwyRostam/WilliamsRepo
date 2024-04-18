9fce0495316f52a2bd37047605cd988cd03588ac
Ewy Rostam <ewy.rost@gmail.com>
2024-04-18 22:01:59 +0200

    Fetch and show dogs on frontpage

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,27 @@
 
+import { useQuery } from 'react-query'
 import './App.css'
+import { getAllDogs } from './api/DogAPI'
 
 function App() {
 
+  const {data, isError, isLoading} = useQuery({
+    queryKey: ['month'], 
+    queryFn: getAllDogs})
+
+    console.log(data);
+
   return (
     <>
-     
+    {isError && <p>An error occured...</p>}
+    {isLoading && <p>Loading...</p>}
+    {data && data.map(dog => 
+      <div>
+        <h1>{dog.name}</h1>
+        <img src={dog.imageUrl} alt="Picture of dog" />
+        <h2>{dog.birthYear}</h2>
+      </div>
+    )}
     </>
   )
 }
